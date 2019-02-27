fe28684a48fe2cdf285088906415d583291ffa29
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-27 22:43:34 +0900

    Set up jest

---
+++ index.js
@@ -0,0 +1,7 @@
+const faker = require('faker');
+
+var randomName = faker.name.findName();
+var randomEmail = faker.internet.email();
+var randomCard = faker.helpers.createCard();
+
+console.log(randomName);
---
+++ tests/index.spec.js
@@ -0,0 +1,5 @@
+describe('test', () => {
+  it('return 2', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
